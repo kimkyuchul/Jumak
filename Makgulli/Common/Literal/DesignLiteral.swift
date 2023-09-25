c2e1df226847903d09c73790fd4d4c4b2e584961
김규철 <rbcjf122@gmail.com>
2023-09-25 20:17:02 +0900

    [SETTING] Literal (#1)

---
+++ Makgulli/Common/Literal/DesignLiteral.swift
@@ -0,0 +1,13 @@
+//
+//  DesignLiteral.swift
+//  Makgulli
+//
+//  Created by 김규철 on 2023/09/25.
+//
+
+import Foundation
+
+enum DesignLiteral {
+    static let cornerRadius: CGFloat = 8
+    static let borderWidth: CGFloat = 1
+}
---
+++ Makgulli/Common/Literal/StringLiteral.swift
@@ -0,0 +1,19 @@
+//
+//  StringLiteral.swift
+//  Makgulli
+//
+//  Created by 김규철 on 2023/09/25.
+//
+
+import Foundation
+
+enum StringLiteral {
+        /// TabBar
+        
+        /// category
+        
+        /// Toast
+        
+        /// Alert
+        static let networkError = "네트워크 에러"
+}
