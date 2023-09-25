d7b6b885e2e148335c506f0f6e862b5193776dfc
김규철 <rbcjf122@gmail.com>
2023-09-25 17:00:30 +0900

    [SETTING] Xcode Project Init

---
+++ Makgulli/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Makgulli
+//
+//  Created by 김규철 on 2023/09/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
