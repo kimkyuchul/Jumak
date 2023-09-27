2f7896f798abc0c4c09c97bd2ee6fb78163c045b
김규철 <rbcjf122@gmail.com>
2023-09-27 18:59:30 +0900

    [ADD] BaseViewController, ViewSetupable (#6)

---
+++ Makgulli/Common/Base/BaseView.swift
@@ -0,0 +1,29 @@
+//
+//  BaseView.swift
+//  Makgulli
+//
+//  Created by 김규철 on 2023/09/27.
+//
+
+import UIKit
+
+class BaseView: UIView, ViewSetupable {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setHierarchy()
+        setConstraints()
+        setLayout()
+    }
+    
+    @available(*, unavailable, message: "remove required init")
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setHierarchy() {}
+    func setConstraints() {}
+    func setLayout() {
+        self.backgroundColor = UIColor.white
+    }
+}
