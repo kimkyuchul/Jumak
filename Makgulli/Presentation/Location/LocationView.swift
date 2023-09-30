ff730763bb3027b00954f3aaea1f78bd78968492
김규철 <rbcjf122@gmail.com>
2023-09-30 15:22:03 +0900

    [ADD] Category collectionView (#8)

--- Makgulli/Presentation/Location/LocationView.swift
+++ Makgulli/Presentation/Location/LocationView.swift
@@ -12,7 +12,7 @@ import RxCocoa
 import NMapsMap
 
 class LocationView: BaseView {
-
+    
     lazy var mapView: NMFMapView = {
         let mapView = NMFMapView()
         mapView.allowsZooming = true
@@ -24,10 +24,18 @@ class LocationView: BaseView {
     let questionButton = QuestionButton()
     let userAddressButton = UserAddressButton()
     
+    lazy var categoryCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: self.createLayout())
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.backgroundColor = .clear
+        collectionView.register(CategoryCollectionViewCell.self, forCellWithReuseIdentifier: "CategoryCollectionViewCell")
+        return collectionView
+    }()
+    
     var locationOverlay: NMFLocationOverlay?
     
     override func setHierarchy() {
-        [mapView, questionButton, userAddressButton].forEach {
+        [mapView, questionButton, userAddressButton, categoryCollectionView].forEach {
             self.addSubview($0)
         }
     }
@@ -47,18 +55,24 @@ class LocationView: BaseView {
             make.centerY.equalTo(questionButton.snp.centerY)
             make.trailing.equalToSuperview().inset(24)
         }
+        
+        categoryCollectionView.snp.makeConstraints { make in
+            make.top.equalTo(userAddressButton.snp.bottom).offset(11)
+            make.leading.trailing.equalToSuperview()
+            make.height.equalTo(46)
+        }
     }
     
     fileprivate func moveCamera(latitude: Double, longitude: Double) {
-            let cameraPosition = NMFCameraPosition(
-                NMGLatLng(lat: latitude,lng: longitude),
-                zoom: self.mapView.zoomLevel
-            )
-            let cameraUpdate = NMFCameraUpdate(position: cameraPosition)
-            
-            cameraUpdate.animation = .easeIn
-            self.mapView.moveCamera(cameraUpdate)
-        }
+        let cameraPosition = NMFCameraPosition(
+            NMGLatLng(lat: latitude,lng: longitude),
+            zoom: self.mapView.zoomLevel
+        )
+        let cameraUpdate = NMFCameraUpdate(position: cameraPosition)
+        
+        cameraUpdate.animation = .easeIn
+        self.mapView.moveCamera(cameraUpdate)
+    }
 }
 
 extension Reactive where Base: LocationView {
@@ -67,5 +81,31 @@ extension Reactive where Base: LocationView {
             let (latitude, longitude) = cameraPosition
             view.moveCamera(latitude: latitude, longitude: longitude)
         }
+    }
+}
+
+
+private extension LocationView {
+    func createLayout() -> UICollectionViewLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .estimated(80), heightDimension: .fractionalHeight(1.0))
+        
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .estimated(80), heightDimension: .absolute(46))
+        
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        section.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 24, bottom: 0, trailing: 24)
+        section.interGroupSpacing = 11
+        
+        let configuration = UICollectionViewCompositionalLayoutConfiguration()
+        configuration.scrollDirection = .horizontal
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        layout.configuration = configuration
+        
+        return layout
     }
 }
--- Makgulli/Presentation/Location/LocationViewController.swift
+++ Makgulli/Presentation/Location/LocationViewController.swift
@@ -30,11 +30,12 @@ final class LocationViewController: BaseViewController {
     override func bind() {
         let input = LocationViewModel.Input(viewDidLoadEvent: Observable.just(()).asObservable(), didSelectMarker: selectMarkerRelay)
         let output = viewModel.transform(input: input)
-                
+        
         output.storeList
             .take(1)
             .withUnretained(self)
             .bind(onNext: { owner, storeList in
+                print(storeList)
                 owner.setUpMarker(storeList: storeList)
             })
             .disposed(by: disposeBag)
@@ -76,6 +77,13 @@ final class LocationViewController: BaseViewController {
             .asDriver(onErrorJustReturn: Void())
             .drive(with: self) { owner, _ in
                 owner.present(QuestionViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        Observable.just(CategoryType.allCases)
+            .bind(to: locationView.categoryCollectionView.rx.items(cellIdentifier: "CategoryCollectionViewCell", cellType: CategoryCollectionViewCell.self)) {
+                index, item, cell in
+                cell.configureCell(item: item)
             }
             .disposed(by: disposeBag)
     }
