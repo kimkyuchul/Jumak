4c9adf5f6b3d6030915a2287f4a77b3223493393
김규철 <rbcjf122@gmail.com>
2023-10-05 17:32:08 +0900

    [CHORE] Base (#10)

--- Makgulli/Common/Base/BaseViewController.swift
+++ Makgulli/Common/Base/BaseViewController.swift
@@ -14,6 +14,15 @@ class BaseViewController: UIViewController, BaseViewControllerProtocol, BaseBind
     
     var disposeBag: DisposeBag = .init()
     
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable, message: "remove required init")
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setHierarchy()
