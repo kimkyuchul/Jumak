39aa25456f52a57413255a6a32083da37b38fc58
김규철 <rbcjf122@gmail.com>
2023-10-09 18:15:17 +0900

    [ADD] DefaultRealmRepisitory 로직 변경 (#10) - willDisplayCell에서 그려질 셀에 대해 필터링을 진행하는 메서드 ex) 다른뷰 이동 후 다시 재진입 시 - rx.modelSelected, rx.items에서 현재 그려진 셀에 대해 필터링을 진행하는 메서드 - 변경된 사항이 존재할 경우를 비교하는 메서드 에피소드도 비교하도록 수정

--- Makgulli/Data/Repositories/DefaultRealmRepository.swift
+++ Makgulli/Data/Repositories/DefaultRealmRepository.swift
@@ -14,6 +14,8 @@ protocol RealmRepository {
     func createStore(_ store: StoreVO) -> Completable
     func updateStore(_ store: StoreVO) -> Completable
     func deleteStore(_ store: StoreVO) -> Completable
+    func updateStoreCellObservable(index: Int, storeList: [StoreVO]) -> Single<StoreVO>
+    func updateStoreCell(store: StoreVO) -> StoreVO?
     func checkContainsStore(id: String) -> Bool
     func shouldUpdateStore(_ store: StoreVO) -> Bool
 }
@@ -81,6 +83,50 @@ final class DefaultRealmRepository: RealmRepository {
         }
     }
     
+    // willDisplayCell에서 그려질 셀에 대해 필터링을 진행하는 메서드 ex) 다른뷰 이동 후 다시 재진입 시
+    func updateStoreCellObservable(index: Int, storeList: [StoreVO]) -> Single<StoreVO> {
+        return Single.create { single in
+            var store = storeList[index]
+            
+            do {
+                try self.realm.write {
+                    let storeTable = self.realm.objects(StoreTable.self).where {
+                        $0.id == store.id
+                    }.first
+                    
+                    if let storeTable {
+                        store.bookmark = storeTable.bookmark
+                        store.rate = storeTable.rate
+                    }
+                }
+                
+                single(.success(store))
+            } catch let error {
+                single(.failure(error))
+            }
+            return Disposables.create()
+        }
+    }
+    
+    // rx.modelSelected, rx.items에서 현재 그려진 셀에 대해 필터링을 진행하는 메서드
+    func updateStoreCell(store: StoreVO) -> StoreVO? {
+        do {
+            var updatedStore = store
+            
+            try realm.write {
+                if let storeTable = realm.objects(StoreTable.self).filter("id == %@", updatedStore.id).first {
+                    updatedStore.bookmark = storeTable.bookmark
+                    updatedStore.rate = storeTable.rate
+                }
+            }
+            
+            return updatedStore
+        } catch {
+            print("Error updating store item: \(error)")
+            return nil
+        }
+    }
+    
     func deleteStore(_ store: StoreVO) -> Completable {
         guard let storeObject = realm.object(
             ofType: StoreTable.self,
@@ -110,14 +156,24 @@ final class DefaultRealmRepository: RealmRepository {
             ofType: StoreTable.self,
             forPrimaryKey: store.id
         ) else { return false }
-        
-        let storeEpisodeIds = Set(store.episode.map { $0.id })
-        let realmEpisodeIds = Set(storeObject.episode.map { $0._id.stringValue })
-
-        if store.rate != storeObject.rate || storeEpisodeIds != realmEpisodeIds {
-            return true
+                
+        let convertEpisodeVO: [EpisodeVO] = storeObject.episode.map { episodeObject in
+            return EpisodeVO(
+                id: episodeObject._id.stringValue,
+                date: episodeObject.date,
+                title: episodeObject.title,
+                content: episodeObject.content,
+                imageURL: episodeObject.imageURL,
+                alcohol: episodeObject.alcohol,
+                mixedAlcohol: episodeObject.mixedAlcohol,
+                drink: episodeObject.drink
+            )
         }
 
+        if store.rate != storeObject.rate || store.bookmark != storeObject.bookmark || store.episode != convertEpisodeVO {
+            return true
+        }
+        
         return false
     }
 }
