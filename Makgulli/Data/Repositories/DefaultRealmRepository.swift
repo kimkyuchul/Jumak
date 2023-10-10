6d40c8cfeca2bc4ea8a644a3a1cc515d7ffdea13
김규철 <rbcjf122@gmail.com>
2023-10-10 21:12:10 +0900

    [CHORE] WriteEpisode 관련 Realm 메서드 추가 (#15) - WriteEpisode -> LocationDetail로 Dismiss될 때 LocationDetail의 viewWillAppear에서 Episode 정보를 업데이트 - 기존에 있던 store에 에피소드를 append - CreateStore 시 에피소드 정보도 같이 Append 하도록 수정

--- Makgulli/Data/Repositories/DefaultRealmRepository.swift
+++ Makgulli/Data/Repositories/DefaultRealmRepository.swift
@@ -14,6 +14,8 @@ protocol RealmRepository {
     func createStore(_ store: StoreVO) -> Completable
     func updateStore(_ store: StoreVO) -> Completable
     func deleteStore(_ store: StoreVO) -> Completable
+    func updateEpisode(id: String, episode: EpisodeTable) -> Completable
+    func updateStoreEpisode(store: StoreVO) -> StoreVO?
     func updateStoreCellObservable(index: Int, storeList: [StoreVO]) -> Single<StoreVO>
     func updateStoreCell(store: StoreVO) -> StoreVO?
     func checkContainsStore(id: String) -> Bool
@@ -58,20 +60,6 @@ final class DefaultRealmRepository: RealmRepository {
                     storeObject.bookmark = store.bookmark
                     storeObject.rate = store.rate
                     storeObject.categoryType = store.categoryType
-                    
-                    storeObject.episode.removeAll()
-                    store.episode.forEach { episodeVO in
-                        let episodeTable = EpisodeTable(
-                            date: episodeVO.date,
-                            comment: episodeVO.comment,
-                            imageURL: episodeVO.imageURL,
-                            alcohol: episodeVO.alcohol,
-                            mixedAlcohol: episodeVO.mixedAlcohol,
-                            drink: episodeVO.drink
-                        )
-                        storeObject.episode.append(episodeTable)
-                    }
-                    
                     self.realm.add(storeObject, update: .modified)
                 }
                 completable(.completed)
@@ -81,7 +69,7 @@ final class DefaultRealmRepository: RealmRepository {
             return Disposables.create()
         }
     }
-    
+        
     // willDisplayCell에서 그려질 셀에 대해 필터링을 진행하는 메서드 ex) 다른뷰 이동 후 다시 재진입 시
     func updateStoreCellObservable(index: Int, storeList: [StoreVO]) -> Single<StoreVO> {
         return Single.create { single in
@@ -129,6 +117,62 @@ final class DefaultRealmRepository: RealmRepository {
         }
     }
     
+    // 기존에 있던 store에 에피소드를 append
+    func updateEpisode(id: String, episode: EpisodeTable) -> Completable {
+        guard let storeObject = realm.object(
+            ofType: StoreTable.self,
+            forPrimaryKey: id
+        ) else { return .empty() }
+        
+        return Completable.create { completable in
+            do {
+                try self.realm.write {
+                    storeObject.episode.append(episode)
+                    self.realm.add(storeObject, update: .modified)
+                }
+                completable(.completed)
+            } catch let error {
+                completable(.error(error))
+            }
+            return Disposables.create()
+        }
+    }
+    
+    // WriteEpisode -> LocationDetail로 Dismiss될 때 LocationDetail의 viewWillAppear에서 Episode 정보를 업데이트
+    func updateStoreEpisode(store: StoreVO) -> StoreVO? {
+        do {
+            var updatedStore = store
+            
+            try realm.write {
+                if let storeTable = realm.objects(StoreTable.self).filter("id == %@", updatedStore.id).first {
+                    
+                    var episodeVOList = [EpisodeVO]()
+                    
+                    let episodeVOArray = storeTable.episode.map { storeObject in
+                        let episodeVO = EpisodeVO(
+                            id: storeObject._id.stringValue,
+                            date: storeObject.date,
+                            comment: storeObject.comment,
+                            imageURL: storeObject.imageURL,
+                            alcohol: storeObject.alcohol,
+                            mixedAlcohol: storeObject.mixedAlcohol,
+                            drink: storeObject.drink)
+                        
+                        return episodeVO
+                    }
+                    
+                    episodeVOList.append(contentsOf: episodeVOArray)
+                    updatedStore.episode = episodeVOList
+                }
+            }
+            
+            return updatedStore
+        } catch {
+            print("Error updating store item: \(error)")
+            return nil
+        }
+    }
+    
     func deleteStore(_ store: StoreVO) -> Completable {
         guard let storeObject = realm.object(
             ofType: StoreTable.self,
@@ -159,19 +203,7 @@ final class DefaultRealmRepository: RealmRepository {
             forPrimaryKey: store.id
         ) else { return false }
                 
-        let convertEpisodeVO: [EpisodeVO] = storeObject.episode.map { episodeObject in
-            return EpisodeVO(
-                id: episodeObject._id.stringValue,
-                date: episodeObject.date,
-                comment: episodeObject.comment,
-                imageURL: episodeObject.imageURL,
-                alcohol: episodeObject.alcohol,
-                mixedAlcohol: episodeObject.mixedAlcohol,
-                drink: episodeObject.drink
-            )
-        }
-
-        if store.rate != storeObject.rate || store.bookmark != storeObject.bookmark || store.episode != convertEpisodeVO {
+        if store.rate != storeObject.rate || store.bookmark != storeObject.bookmark {
             return true
         }
         
@@ -181,6 +213,22 @@ final class DefaultRealmRepository: RealmRepository {
 
 private extension StoreVO {
     func makeStoreTable() -> StoreTable {
+        let episodeList = List<EpisodeTable>()
+        
+        let episodeTableArray = self.episode.map { episodeVO in
+            let episodeTable = EpisodeTable()
+            episodeTable.comment = episodeVO.comment
+            episodeTable.date = episodeVO.date
+            episodeTable.alcohol = episodeVO.alcohol
+            episodeTable.mixedAlcohol = episodeVO.mixedAlcohol
+            episodeTable.imageURL = episodeVO.imageURL
+            episodeTable.drink = episodeVO.drink
+            return episodeTable
+        }
+        
+        episodeList.append(objectsIn: episodeTableArray)
+        
+        
         let storeTable = StoreTable(id: self.id,
                                     placeName: self.placeName,
                                     distance: self.distance,
@@ -193,7 +241,9 @@ private extension StoreVO {
                                     y: self.y,
                                     categoryType: self.categoryType,
                                     rate: self.rate,
-                                    bookmark: self.bookmark)
+                                    bookmark: self.bookmark,
+                                    episode: episodeList
+        )
         return storeTable
     }
 }
