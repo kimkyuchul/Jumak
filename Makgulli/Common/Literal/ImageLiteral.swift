05821fbc219072fc0e38d0954de728d587691408
김규철 <rbcjf122@gmail.com>
2023-10-17 19:08:53 +0900

    [ADD] titleArrowDownIcon (#17)

--- Makgulli/Common/Literal/ImageLiteral.swift
+++ Makgulli/Common/Literal/ImageLiteral.swift
@@ -56,6 +56,7 @@ enum ImageLiteral {
     static var arrowDownIcon : UIImage { .load(systemName: "chevron.down") }
     static var episodeDefaultImage : UIImage { .load(systemName: "party.popper.fill") }
     static var deleteEpisodeIcon : UIImage { .load(systemName: "trash.fill") }
+    static var titleArrowDownIcon : UIImage { .load(systemName: "arrowtriangle.down.circle") }
 }
 
 extension UIImage {
