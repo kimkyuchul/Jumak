f214ca7c0d140970308755c4709c13c9f3680913
김규철 <rbcjf122@gmail.com>
2023-10-19 01:30:39 +0900

    [ADD] NotificationCenterHandler rx 구현 (#17)

---
+++ Makgulli/Common/Extension/Rx/NotificationCenterHandler.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationCenterHandler.swift
+//  Makgulli
+//
+//  Created by 김규철 on 2023/10/18.
+//
+
+import Foundation
+import RxSwift
+
+protocol NotificationCenterHandler {
+    var name: Notification.Name { get }
+}
+
+extension NotificationCenterHandler {
+    func addObserver() -> Observable<Any?> {
+        return NotificationCenter.default.rx.notification(name).map { $0.object }
+    }
+
+    func post(object: Any? = nil) {
+        NotificationCenter.default.post(name: name, object: object, userInfo: nil)
+    }
+}
+
+enum NotificationCenterManager: NotificationCenterHandler {
+    case filterStore
+
+    var name: Notification.Name {
+        switch self {
+        case .filterStore:
+            return Notification.Name("NotificationCenterManager.filterStore")
+        }
+    }
+}
