66c711a085b7e42cad42f0f6dc248b497d387b6f
김규철 <rbcjf122@gmail.com>
2023-10-23 16:20:31 +0900

    [CHORE] FilterColletionVIewCell 디자인 수정 (#19)

--- Makgulli/Presentation/Favorite/View/Cell/FilterCollectionViewCell.swift
+++ Makgulli/Presentation/Favorite/View/Cell/FilterCollectionViewCell.swift
@@ -23,8 +23,9 @@ final class FilterCollectionViewCell: BaseCollectionViewCell {
     }()
     private let storeTitleLabel: UILabel = {
         let label = UILabel()
-        label.font = UIFont.boldLineSeed(size: ._18)
+        label.font = UIFont.boldLineSeed(size: UIDevice.current.hasNotch ? ._16 : ._18)
         label.numberOfLines = 2
+        label.sizeToFit()
         label.textColor = .black
         return label
     }()
@@ -35,14 +36,62 @@ final class FilterCollectionViewCell: BaseCollectionViewCell {
         label.textColor = .darkGray
         return label
     }()
-    private let rateBadge = StoreBadge(image: ImageLiteral.starCircleIcon)
-    private let distanceBadge = StoreBadge(image: ImageLiteral.storeLocationIcon)
-    private lazy var badgeStackView: UIStackView = {
+    private lazy var titleStackView: UIStackView = {
         let stackView = UIStackView()
-        stackView.addArrangedSubviews(rateBadge, distanceBadge)
-        stackView.axis = .horizontal
-        stackView.spacing = 14
+        stackView.addArrangedSubviews(storeTitleLabel, addressLabel)
+        stackView.axis = .vertical
+        stackView.spacing = 2
         stackView.distribution = .fill
+        return stackView
+    }()
+    private let rateStatusLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.regularLineSeed(size: ._14)
+        return label
+    }()
+    private let rateImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = ImageLiteral.fillStarIcon
+        return imageView
+    }()
+    private let rateLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.regularLineSeed(size: ._14)
+        return label
+    }()
+    private lazy var rateStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.addArrangedSubviews(rateImageView, rateLabel)
+        stackView.axis = .horizontal
+        stackView.spacing = 1
+        stackView.distribution = .fill
+        return stackView
+    }()
+    private let episodeImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.tintColor = .white
+        imageView.image = ImageLiteral.episodeDefaultImage
+        return imageView
+    }()
+    private let episodeLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = UIFont.boldLineSeed(size: ._14)
+        return label
+    }()
+    private lazy var episodeStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.addArrangedSubviews(episodeImageView, episodeLabel)
+        stackView.axis = .horizontal
+        stackView.spacing = 1
+        stackView.distribution = .fill
+        stackView.backgroundColor = .blue
+        stackView.layer.cornerRadius = 4
+        stackView.clipsToBounds = true
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 5, leading: 6, bottom: 5, trailing: 6)
         return stackView
     }()
     private let bookmarkImageView: UIImageView = {
@@ -71,7 +120,7 @@ final class FilterCollectionViewCell: BaseCollectionViewCell {
     override func setHierarchy() {
         self.addSubview(containerView)
         
-        [logoImageView, storeTitleLabel, addressLabel, badgeStackView, bookmarkImageView, lineView].forEach {
+        [logoImageView, titleStackView, rateStatusLabel, rateStackView, episodeStackView, bookmarkImageView, lineView].forEach {
             containerView.addSubview($0)
         }
     }
@@ -84,31 +133,7 @@ final class FilterCollectionViewCell: BaseCollectionViewCell {
         logoImageView.snp.makeConstraints { make in
             make.centerY.equalToSuperview()
             make.leading.equalToSuperview().inset(15)
-            make.size.equalTo(100)
-        }
-        
-        storeTitleLabel.snp.makeConstraints { make in
-            make.top.equalTo(logoImageView.snp.top).offset(5)
-            make.leading.equalTo(logoImageView.snp.trailing).offset(12)
-            make.trailing.equalToSuperview().inset(15).priority(.high)
-        }
-        
-        addressLabel.snp.makeConstraints { make in
-            make.top.equalTo(storeTitleLabel.snp.bottom).offset(2).priority(.high)
-            make.leading.equalTo(storeTitleLabel.snp.leading)
-            make.trailing.equalTo(storeTitleLabel.snp.trailing)
-        }
-        
-        badgeStackView.snp.makeConstraints { make in
-            make.top.equalTo(addressLabel.snp.bottom).offset(5).priority(.low)
-            make.leading.equalTo(storeTitleLabel.snp.leading)
-            make.bottom.equalToSuperview().inset(18)
-        }
-        
-        bookmarkImageView.snp.makeConstraints { make in
-            make.size.equalTo(38)
-            make.trailing.equalToSuperview().inset(10)
-            make.bottom.equalTo(badgeStackView.snp.bottom).offset(2)
+            make.size.equalTo(UIDevice.current.hasNotch ? 105 : 115)
         }
         
         lineView.snp.makeConstraints { make in
@@ -116,21 +141,55 @@ final class FilterCollectionViewCell: BaseCollectionViewCell {
             make.leading.trailing.equalToSuperview().inset(30)
             make.bottom.equalToSuperview()
         }
+                
+        titleStackView.snp.makeConstraints { make in
+            make.top.equalTo(logoImageView.snp.top)
+            make.leading.equalTo(logoImageView.snp.trailing).offset(12)
+            make.trailing.equalTo(bookmarkImageView.snp.leading).offset(4).priority(.high)
+        }
+        
+        rateStatusLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleStackView.snp.bottom).offset(10)
+            make.leading.equalTo(storeTitleLabel.snp.leading)
+        }
+        
+        rateImageView.snp.makeConstraints { make in
+            make.size.equalTo(15)
+        }
+                
+        rateStackView.snp.makeConstraints { make in
+            make.leading.equalTo(rateStatusLabel.snp.trailing).offset(4)
+            make.centerY.equalTo(rateStatusLabel.snp.centerY)
+        }
+        
+        episodeImageView.snp.makeConstraints { make in
+            make.size.equalTo(15)
+        }
+        
+        episodeStackView.snp.makeConstraints { make in
+            make.top.equalTo(rateStackView.snp.bottom).offset(4).priority(.high)
+            make.leading.equalTo(titleStackView.snp.leading)
+            make.bottom.equalTo(logoImageView.snp.bottom).priority(.low)
+        }
+                
+        bookmarkImageView.snp.makeConstraints { make in
+            make.size.equalTo(48)
+            make.trailing.equalToSuperview().inset(10)
+            make.centerY.equalToSuperview()
+        }
     }
     
-    private func setSelected(isSelected: Bool) {
-        if isSelected {
-            containerView.backgroundColor = UIColor.brown
-            storeTitleLabel.textColor = .white
-            addressLabel.textColor = .pink
-            rateBadge.setSeletedBadgeColor(color: .white)
-            distanceBadge.setSeletedBadgeColor(color: .white)
+    private func setRateStatus(rate: Int) {
+        if rate == 0 {
+            rateStatusLabel.text = "평가하지 않음"
+            rateStatusLabel.textColor = .deepDarkGray
+            rateImageView.tintColor = .deepDarkGray
+            rateLabel.textColor = .deepDarkGray
         } else {
-            containerView.backgroundColor = .white
-            storeTitleLabel.textColor = .black
-            addressLabel.textColor = .darkGray
-            rateBadge.setSeletedBadgeColor(color: .black)
-            distanceBadge.setSeletedBadgeColor(color: .black)
+            rateStatusLabel.text = "평가함"
+            rateStatusLabel.textColor = .deepYellow
+            rateImageView.tintColor = .deepYellow
+            rateLabel.textColor = .deepYellow
         }
     }
 }
@@ -140,9 +199,9 @@ extension FilterCollectionViewCell {
         logoImageView.image = item.categoryType.logoImage.resize(newWidth: 50)
         storeTitleLabel.text = item.placeName
         addressLabel.text = item.addressName
-        rateBadge.setBadgeTitle(text: "\(item.rate)점")
-        distanceBadge.setBadgeTitle(text: "\(item.distance)M")
+        rateLabel.text = "\(item.rate)점"
+        setRateStatus(rate: item.rate)
+        episodeLabel.text = "\(item.episode.count)개 에피소드 존재"
         bookmarkImageView.tintColor = item.bookmark ? .pink : .gray
-        
     }
 }
