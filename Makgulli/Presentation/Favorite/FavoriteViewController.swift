f16ac0caa7248347d3eac1591ec52e28fe3b7474
김규철 <rbcjf122@gmail.com>
2023-10-23 16:19:39 +0900

    [CHORE] Notch 유무 UI 대응 (#19)

--- Makgulli/Presentation/Favorite/FavoriteViewController.swift
+++ Makgulli/Presentation/Favorite/FavoriteViewController.swift
@@ -150,7 +150,7 @@ final class FavoriteViewController: BaseViewController {
         let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
         
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(0.2))
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(UIDevice.current.hasNotch ? 0.2 : 0.3))
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         
         let section = NSCollectionLayoutSection(group: group)
