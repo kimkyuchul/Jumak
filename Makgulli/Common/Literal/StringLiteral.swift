c36d75810f0b5be817a489b21c9408fdd52067d2
김규철 <rbcjf122@gmail.com>
2023-10-25 19:09:00 +0900

    [CHORE] StringLiteral (#19)

--- Makgulli/Common/Literal/StringLiteral.swift
+++ Makgulli/Common/Literal/StringLiteral.swift
@@ -8,24 +8,27 @@
 import Foundation
 
 enum StringLiteral {
-        /// TabBar
-        static let home = "홈"
-        static let location = "막걸리찾기"
-        static let Favorite = "즐겨찾는곳"
-        
-        /// category
-        static let MAKGULLI = "막걸리"
-        static let PAJEON = "with 파전"
-        static let BOSSAM = "with 보쌈"
+    /// TabBar
+    static let home = "홈"
+    static let location = "막걸리찾기"
+    static let Favorite = "즐겨찾는곳"
     
-        ///Button Title
-        static let reSearchButton = "이 지역에서 다시 검색"
-        
-        /// Toast
-        
-        /// Alert
-        static let networkError = "네트워크 에러"
+    /// category
+    static let MAKGULLI = "막걸리"
+    static let PAJEON = "with 파전"
+    static let BOSSAM = "with 보쌈"
     
-        /// nil
-        static let noPhoneNumberMessage = "전화번호 정보가 없어요"
+    ///Button Title
+    static let reSearchButton = "이 지역에서 다시 검색"
+    
+    /// Toast
+    
+    /// Alert
+    static let networkError = "네트워크 에러"
+    
+    /// nil
+    static let noPhoneNumberMessage = "전화번호 정보가 없어요"
+    
+    /// email
+    static let email = "rbcjf122@gmail.com"
 }
