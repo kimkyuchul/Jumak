1eee418583ad5547f2322bdf6cab677055577e43
김규철 <rbcjf122@gmail.com>
2023-10-25 20:41:27 +0900

    [ADD] RealmRepository 프로토콜 -> RepisitoryInterface로 이동 (#19)

--- Makgulli/Data/Repositories/DefaultRealmRepository.swift
+++ Makgulli/Data/Repositories/DefaultRealmRepository.swift
@@ -10,26 +10,6 @@ import Foundation
 import RxSwift
 import RealmSwift
 
-protocol RealmRepository {
-    func fetchBookmarkStore(sortAscending: Bool, categoryFilter: CategoryFilterType) -> Single<[StoreVO]>
-    func fetchBookmarkStoreSortedByRating(sortAscending: Bool, categoryFilter: CategoryFilterType) -> Single<[StoreVO]>
-    func fetchStoreSortedByRating(sortAscending: Bool, categoryFilter: CategoryFilterType) -> Single<[StoreVO]>
-    func fetchStoreSortedByEpisodeCount(sortAscending: Bool, categoryFilter: CategoryFilterType) -> Single<[StoreVO]>
-    func fetchBookmarkStoreSortedByEpisodeCount(sortAscending: Bool, categoryFilter: CategoryFilterType) -> Single<[StoreVO]>
-    func fetchStoreSortedByName(sortAscending: Bool, categoryFilter: CategoryFilterType) -> Single<[StoreVO]>
-    func createStoreTable(_ store: StoreTable) -> Completable
-    func createStore(_ store: StoreVO) -> Completable
-    func updateStore(_ store: StoreVO) -> Completable
-    func updateEpisode(id: String, episode: EpisodeTable) -> Completable
-    func updateStoreEpisode(store: StoreVO) -> StoreVO?
-    func updateStoreCellObservable(index: Int, storeList: [StoreVO]) -> Single<StoreVO>
-    func updateStoreCell(store: StoreVO) -> StoreVO?
-    func deleteStore(_ store: StoreVO) -> Completable
-    func deleteEpisode(id: String, episodeId: String) -> Completable
-    func checkContainsStore(id: String) -> Bool
-    func shouldUpdateStore(_ store: StoreVO) -> Bool
-}
-
 final class DefaultRealmRepository: RealmRepository {
     private let realm: Realm
     
@@ -98,6 +78,9 @@ final class DefaultRealmRepository: RealmRepository {
         return Single.create { single in
             let storeValue = self.realm.objects(StoreTable.self)
                 .sorted(byKeyPath: "date", ascending: false)
+                .filter { storeTable in
+                         return storeTable.episode.count > 0
+                     }
                 .sorted { (store1, store2) in
                     let episodeCount1 = store1.episode.count
                     let episodeCount2 = store2.episode.count
@@ -123,6 +106,9 @@ final class DefaultRealmRepository: RealmRepository {
             let storeValue = self.realm.objects(StoreTable.self)
                 .sorted(byKeyPath: "date", ascending: false)
                 .filter("bookmark == %@", true)
+                .filter { storeTable in
+                         return storeTable.episode.count > 0
+                     }
                 .sorted { (store1, store2) in
                     let episodeCount1 = store1.episode.count
                     let episodeCount2 = store2.episode.count
