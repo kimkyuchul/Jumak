2c5e6db32a00c8f074ea2ebbfba54f20f4b286d1
김규철 <rbcjf122@gmail.com>
2023-10-25 19:09:19 +0900

    [ADD] UIButton Extension setUnderline (#19)

--- Makgulli/Common/Extension/UIButton+.swift
+++ Makgulli/Common/Extension/UIButton+.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+extension UIButton {
+    func setUnderline() {
+        guard let title = title(for: .normal) else { return }
+        let attributedString = NSMutableAttributedString(string: title)
+        attributedString.addAttribute(.underlineStyle,
+                                      value: NSUnderlineStyle.single.rawValue,
+                                      range: NSRange(location: 0, length: title.count)
+        )
+        setAttributedTitle(attributedString, for: .normal)
+    }
+}
+
 extension UIButton.Configuration {
     mutating func setAttributedTitle(title: String, font: UIFont, color: UIColor) {
         let attributedTitle = NSAttributedString(string: title, attributes: [
