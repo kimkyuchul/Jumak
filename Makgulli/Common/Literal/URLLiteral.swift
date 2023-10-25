ed23ce5d4cdf32e04a5f89713c46fa70af9ff8a4
김규철 <rbcjf122@gmail.com>
2023-10-25 19:08:46 +0900

    [ADD] URLLiteral (#19)

---
+++ Makgulli/Common/Literal/URLLiteral.swift
@@ -0,0 +1,14 @@
+//
+//  URLLiteral.swift
+//  Makgulli
+//
+//  Created by 김규철 on 2023/10/25.
+//
+
+import Foundation
+
+enum URLLiteral {
+    static let policy = "https://gravel-drink-4ae.notion.site/4539425b60ca4215841de660de3b079b?pvs=4"
+    static let openSourceInfo = "https://gravel-drink-4ae.notion.site/Libraries-We-Use-adc8e5a7d008430eba23648ecf1b63ba?pvs=4"
+    static let instagram = "https://www.instagram.com/jumak.official/?igshid=YTQwZjQ0NmI0OA%3D%3D&utm_source=qr"
+}
