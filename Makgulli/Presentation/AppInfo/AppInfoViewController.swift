40f730b0c3fada01cf46d654ff17b1dae6356711
김규철 <rbcjf122@gmail.com>
2023-10-25 19:10:47 +0900

    [CHORE] AppInfoViewController enum으로 리펙토링 - enum 섹션 안에 enum 배열 셀로 앱버전 셀에서만 버전 정보 노출되도록 리펙토링 - 셀 별 이벤트 구현 - RxTableViewSectionedReloadDataSource에서는 테이블뷰 섹션 헤더 커스텀 되지 않는 문제 -> rx.delegate로 커스텀

--- Makgulli/Presentation/AppInfo/AppInfoViewController.swift
+++ Makgulli/Presentation/AppInfo/AppInfoViewController.swift
@@ -10,22 +10,18 @@ import UIKit
 import RxSwift
 import RxCocoa
 import RxDataSources
-
+import SafariServices
 
 final class AppInfoViewController: BaseViewController {
     
     enum AppInfoType: CaseIterable, CustomStringConvertible {
         case appInfo
         
-        var contents: [String] {
+        var contents: [AppInfoSection] {
             switch self {
             case .appInfo:
-                return ["문의하기", "개인정보 처리방침", "오픈소스 사용정보", "버전정보"]
+                return [.inquiry, .privacyPolicy, .openSourceInfo, .versionInfo]
             }
-        }
-        
-        var numberOfRowInSections: Int {
-            return contents.count
         }
         
         var description: String {
@@ -36,26 +32,46 @@ final class AppInfoViewController: BaseViewController {
         }
     }
     
+    enum AppInfoSection: String, CustomStringConvertible {
+        case inquiry = "문의하기"
+        case privacyPolicy = "개인정보 처리방침"
+        case openSourceInfo = "오픈소스 사용정보"
+        case versionInfo = "버전정보"
+        
+        var description: String {
+            return self.rawValue
+        }
+    }
+    
     private lazy var appInfoTableView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.rowHeight = 55
         tableView.showsVerticalScrollIndicator = false
-        tableView.register(FilterTableViewCell.self, forCellReuseIdentifier: "FilterTableViewCell")
+        tableView.register(AppInfoTableViewCell.self, forCellReuseIdentifier: "AppInfoTableViewCell")
         tableView.separatorStyle = .none
         tableView.backgroundColor = .white
         return tableView
     }()
     
-    private typealias SectionOfAppInfo = SectionModel<AppInfoType, String>
+    private typealias SectionOfAppInfo = SectionModel<AppInfoType, AppInfoSection>
     private var dataSource = RxTableViewSectionedReloadDataSource<SectionOfAppInfo>( configureCell: { (dataSource, tableView, indexPath, item) in
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "FilterTableViewCell", for: indexPath) as? FilterTableViewCell else { return UITableViewCell() }
-        cell.titleLabel.text = item
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "AppInfoTableViewCell", for: indexPath) as? AppInfoTableViewCell else { return UITableViewCell() }
+        
+        cell.configureCell(item: item.description)
+        
+        let section = AppInfoType.appInfo
+        
+        if case .versionInfo = section.contents[indexPath.row] {
+            cell.configureAppVersion(version: Bundle.main.appVersion)
+        }
+        
         return cell
     }, titleForHeaderInSection: { dataSource, sectionIndex in
         return dataSource[sectionIndex].model.description
-    })
+    }
+    )
     
-    private let sections: [SectionModel<AppInfoType, String>] = AppInfoType.allCases.map { section in
+    private let sections: [SectionModel<AppInfoType, AppInfoSection>] = AppInfoType.allCases.map { section in
         SectionModel(model: section, items: section.contents)
     }
     
@@ -72,6 +88,37 @@ final class AppInfoViewController: BaseViewController {
         Observable.just(sections)
             .bind(to: appInfoTableView.rx.items(dataSource: dataSource))
             .disposed(by: disposeBag)
+        
+        appInfoTableView.rx.setDelegate(self)
+            .disposed(by: disposeBag)
+    }
+    
+    override func bindAction() {
+        appInfoTableView.rx.itemSelected
+            .withUnretained(self)
+            .bind(onNext: { owner, indexPath in
+                let section = AppInfoType.appInfo
+                
+                switch section.contents[indexPath.row] {
+                case .inquiry:
+                    owner.navigationController?.pushViewController(InquiryViewController(), animated: true)
+                case .privacyPolicy:
+                    guard
+                        let url = URL(string: URLLiteral.policy.trimmingWhitespace()), UIApplication.shared.canOpenURL(url)
+                    else { return }
+                    let safariViewController = SFSafariViewController(url: url)
+                    owner.present(safariViewController, animated: true)
+                case .openSourceInfo:
+                    guard
+                        let url = URL(string: URLLiteral.openSourceInfo.trimmingWhitespace()), UIApplication.shared.canOpenURL(url)
+                    else { return }
+                    let safariViewController = SFSafariViewController(url: url)
+                    owner.present(safariViewController, animated: true)
+                case .versionInfo:
+                    owner.showToast(message: "최신 버전의 주막입니다.")
+                }
+            })
+            .disposed(by: disposeBag)
     }
     
     override func setHierarchy() {
@@ -81,6 +128,30 @@ final class AppInfoViewController: BaseViewController {
     override func setConstraints() {
         appInfoTableView.snp.makeConstraints { make in
             make.edges.equalTo(view.safeAreaLayoutGuide.snp.edges)
+        }
+    }
+    
+    override func setLayout() {
+        view.backgroundColor = .white
+    }
+}
+
+extension AppInfoViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        guard let header = view as? UITableViewHeaderFooterView else { return }
+        header.textLabel?.font = UIFont.boldLineSeed(size: ._24)
+        header.textLabel?.textColor = .black
+        
+        if let label = header.textLabel {
+            label.translatesAutoresizingMaskIntoConstraints = false
+            label.leadingAnchor.constraint(equalTo: header.leadingAnchor, constant: 18).isActive = true
+            label.centerYAnchor.constraint(equalTo: header.centerYAnchor).isActive = true
+        }
+        
+        header.textLabel?.sizeToFit()
+        
+        func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+            return 100
         }
     }
 }
