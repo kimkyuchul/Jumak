b900e86f22ed9264dc14a4efe80b48500d6dc97e
김규철 <rbcjf122@gmail.com>
2023-11-06 21:52:12 +0900

    [ADD] AppDIContainer (#21)

---
+++ Makgulli/Application/DIContainer/AppDIContainer.swift
@@ -0,0 +1,25 @@
+//
+//  AppDIContainer.swift
+//  Makgulli
+//
+//  Created by 김규철 on 2023/11/03.
+//
+
+import Foundation
+
+
+final class AppDIContainer {
+    
+    static let shared = AppDIContainer()
+    
+    private init() {}
+    
+    lazy var networkManager = NetworkManager<LocationAPI>()
+    
+    func makeLocationDIContainer() -> LocationDIContainer {
+        let dependencies = LocationDIContainer.Dependencies(
+            networkManager: networkManager
+        )
+        return LocationDIContainer(dependencies: dependencies)
+    }
+}
