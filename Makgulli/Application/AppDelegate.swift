c356f8c95cb28d961fa0c737363d4f62d0afd19b
김규철 <rbcjf122@gmail.com>
2023-11-14 00:57:35 +0900

    [ADD] FirebaseApp 초기화 (#23)

--- Makgulli/Application/AppDelegate.swift
+++ Makgulli/Application/AppDelegate.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+
+import FirebaseCore
 import NMapsMap
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         NMFAuthManager.shared().clientId = Bundle.main.naverMapsClientID
+        FirebaseApp.configure()
         setNavigationAppearance()
         return true
     }
