d7e470c041c170721920fabae19df93555a9e911
kimkyuchul <rbcjf122@gmail.com>
2024-12-13 16:57:27 +0900

    [ADD] NetworkManager AnyPublisher 리턴 타입 추가

--- Makgulli/Data/Network/Foundation/NetworkManager.swift
+++ Makgulli/Data/Network/Foundation/NetworkManager.swift
@@ -6,24 +6,34 @@
 //
 
 import Foundation
+import Combine
 
 import RxSwift
 import Alamofire
 
-struct NetworkManager<T: TargetType> {
+protocol NetworkSessionable<APIType> {
+    associatedtype APIType: TargetType
     
-    func request<T: Decodable>(_ target: TargetType, type: T.Type) -> Single<T> {
+    func request<Model: Decodable>(_ target: APIType, type: Model.Type) -> Single<Model>
+    func request<Model: Decodable>(_ target: APIType, type: Model.Type) -> AnyPublisher<Model, Error>
+}
+
+struct NetworkManager<APIType: TargetType>: NetworkSessionable {
+    private let decoder: JSONDecoder
+    
+    init() {
+        self.decoder = JSONDecoder()
+    }
+    
+    func request<Model: Decodable>(_ target: APIType, type: Model.Type) -> Single<Model> {
         
         return Single.create(subscribe: { single in
             AF.request(target).responseData { response in
                 switch response.result {
                 case .success(let value):
-                    guard let statusCode = response.response?.statusCode else { return }
-                    let isSuccessful = (200..<300).contains(statusCode)
-                    
-                    if isSuccessful {
+                    if validate(response.response) {
                         do {
-                            let data = try JSONDecoder().decode(T.self, from: value)
+                            let data = try decoder.decode(Model.self, from: value)
                             single(.success(data))
                         } catch {
                             dump(error.localizedDescription)
@@ -39,4 +49,36 @@ struct NetworkManager<T: TargetType> {
             return Disposables.create()
         })
     }
+    
+    func request<Model: Decodable>(_ target: APIType, type: Model.Type) -> AnyPublisher<Model, Error> {
+        Future<Model, Error> { promise in
+            AF.request(target).responseData { response in
+                switch response.result {
+                case .success(let value):
+                    if validate(response.response) {
+                        do {
+                            let data = try decoder.decode(Model.self, from: value)
+                            promise(.success(data))
+                        } catch {
+                            dump(error.localizedDescription)
+                            promise(.failure(NetworkError.decodingError))
+                        }
+                    } else {
+                        promise(.failure(NetworkError.isNotSuccessful(statusCode: response.response?.statusCode ?? 500)))
+                    }
+                    
+                case .failure(let failure):
+                    promise(.failure(NetworkError.underlyingError(message: failure.localizedDescription)))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
 }
+
+private extension NetworkManager {
+    func validate(_ response: HTTPURLResponse?) -> Bool {
+        guard let statusCode = response?.statusCode else { return false }
+        return (200..<300).contains(statusCode)
+    }
+}
+
