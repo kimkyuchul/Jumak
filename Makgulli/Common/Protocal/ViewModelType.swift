a0aec0e274e0ba279fa199594b2efcd8e4a590a8
kimkyuchul <rbcjf122@gmail.com>
2024-12-13 17:09:16 +0900

    [FIX] ViewModelType 수정 (#35)

--- Makgulli/Common/Protocal/ViewModelType.swift
+++ Makgulli/Common/Protocal/ViewModelType.swift
@@ -7,14 +7,10 @@
 
 import Foundation
 
-import RxSwift
-
 protocol ViewModelType {
 
     associatedtype Input
     associatedtype Output
-
-    var disposeBag: DisposeBag { get set }
 
     func transform(input: Input) -> Output
 }
